81a3c61643f152c93cb780b9f324d3cb7f9c080b
jinzhihao.10 <jinzhihao.10@bytedance.com>
2021-04-06 11:58:25 +0800

    new version

---
+++ gdt/build/generated/source/buildConfig/debug/com/ifmvo/togetherad/gdt/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ifmvo.togetherad.gdt;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ifmvo.togetherad.gdt";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "4.1.8-4.333.1203";
+}
